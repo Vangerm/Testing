021ea8580a5c7df497ee75376bdd58a1fcc24559
ANTON <ANTON-1996@yandex.ru>
2025-05-24 21:46:59 +0300

    изучаем диалог

--- mini_bot/__main__.py
+++ mini_bot/__main__.py
@@ -1,11 +1,12 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, User
 from aiogram_dialog import Dialog, DialogManager, StartMode, Window, setup_dialogs
-from aiogram_dialog.widgets.text import Const  # Здесь будем импортировать нужные виджеты
+from aiogram_dialog.widgets.kbd import Button, Row
+from aiogram_dialog.widgets.text import Const, Format
 from environs import Env
 
 env = Env()
@@ -16,33 +17,63 @@ BOT_TOKEN = env('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
+router = Router()
+
 
 class StartSG(StatesGroup):
     start = State()
 
 
-async def some_handler(callback: CallbackQuery, dialog_manager: DialogManager):  # Здесь будут хэндлеры
-    pass
+# Хэндлер, обрабатывающий нажатие на кнопку 'Да'
+async def yes_click_process(callback: CallbackQuery,
+                            widget: Button,
+                            dialog_manager: DialogManager):
+    await callback.message.edit_text(
+        text='<b>Прекрасно!</b>\n\nНадеюсь, вы найдете в этом курсе что-то '
+             'новое и полезное для себя!'
+    )
+    await dialog_manager.done()
 
 
-async def some_getter(**kwargs):  # Здесь будем создавать нужные геттеры
-    pass
+# Хэндлер, обрабатывающий нажатие на кнопку 'Нет'
+async def no_click_process(callback: CallbackQuery,
+                           widget: Button,
+                           dialog_manager: DialogManager):
+    await callback.message.edit_text(
+        text='<b>Попробуйте!</b>\n\nСкорее всего, вам понравится!'
+    )
+    await dialog_manager.done()
 
 
-# Это стартовый диалог
+# Это геттер
+async def get_username(event_from_user: User, **kwargs):
+    return {'username': event_from_user.username}
+
+
 start_dialog = Dialog(
     Window(
-        ...  # Здесь будем добавлять виджеты и геттеры
+        Format(text='Привет, <b>{username}</b>!\n'),
+        Const(
+            text='Пробовали ли вы уже писать ботов с использованием '
+                 'библиотеки <code>aiogram_dialog</code>?'
+        ),
+        Row(
+            Button(text=Const('✅ Да'), id='yes', on_click=yes_click_process),
+            Button(text=Const('✖️ Нет'), id='no', on_click=no_click_process),
+        ),
+        getter=get_username,
+        state=StartSG.start,
     ),
 )
 
 
-# Этот классический хэндлер будет срабатывать на команду /start
-@dp.message(CommandStart())
+# Это классический хэндлер, который будет срабатывать на команду /start
+@router.message(CommandStart())
 async def command_start_process(message: Message, dialog_manager: DialogManager):
     await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
 
 
+dp.include_router(router)
 dp.include_router(start_dialog)
 setup_dialogs(dp)
 dp.run_polling(bot)
