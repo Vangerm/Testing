c5e26dfb2eb4b9c3ab34806b48bbd8301b81d0ae
ANTON <ANTON-1996@yandex.ru>
2025-07-08 23:05:13 +0300

    ...

--- mini_bot/__main__.py
+++ mini_bot/__main__.py
@@ -22,52 +22,10 @@ dp = Dispatcher()
 router = Router()
 
 
-class StartSG(StatesGroup):
-    start = State()
-
-
-class SecondDialogSG(StatesGroup):
-    start = State()
-
-
-async def go_start(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
-
-
-async def start_second(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
-    await dialog_manager.start(state=SecondDialogSG.start)
-
-
-async def username_getter(dialog_manager: DialogManager, event_from_user: User, **kwargs):
-    return {'username': event_from_user.username or 'Stranger'}
-
-
-start_dialog = Dialog(
-    Window(
-        Format('<b>Привет, {username}!</b>\n'),
-        Const('Нажми на кнопку,\nчтобы перейти во второй диалог 👇'),
-        Button(Const('Кнопка'), id='go_second', on_click=start_second),
-        getter=username_getter,
-        state=StartSG.start
-    ),
-)
-
-second_dialog = Dialog(
-    Window(
-        Const('Нажми на кнопку,\nчтобы вернуться в стартовый диалог 👇'),
-        Button(Const('Кнопка'), id='button_start', on_click=go_start),
-        state=SecondDialogSG.start
-    ),
-)
-
-
 @dp.message(CommandStart())
 async def command_start_process(message: Message):
     print(message.text)
 
 
-
 dp.include_router(router)
-dp.include_routers(start_dialog, second_dialog)
-setup_dialogs(dp)
 dp.run_polling(bot)
